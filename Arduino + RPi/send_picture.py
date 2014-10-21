d0a78e4200713a38e93a1dea7f172f9d06de2ebf
SanderSwitsers <sswitsersgmail.com>
2014-10-21 12:41:26 +0000

    zinnetje toegevoegd

--- Arduino + RPi/send_picture.py
+++ Arduino + RPi/send_picture.py
@@ -1,14 +1,18 @@
 import base64 
 import json 
 from socketIO_client import SocketIO
-
+import requests
 
 global lrs
 lrs = 0
 start = {'purpose':'realtime-sender','groupID':'CWB2','userID':'r0369676'}
 socketIO = SocketIO('dali.cs.kuleuven.be',8080)
 
-
+def on_response(*args):
+    global lrs
+    print 'server message is',args
+    lrs = args
+    
 def send_data():
     socketIO.on('server_message',on_response)
     socketIO.emit('start',json.dumps(start),on_response)
@@ -22,8 +26,10 @@ def send_data():
 
         
     file = open("/home/pi/Desktop/fotos/img1.jpg", "rb").read().encode("base64") 
-    test = json.dumps({"imageName" : "imgnaam1.jpg", "tripID" : str(tripID), "userID" : r0369676, "raw" : file}) 
+    test = json.dumps({"imageName" : "imgnaam1.jpg", "tripID" : str(tripID), "userID" : "r0369676", "raw" : file}) 
     print test 
     url = "http://dali.cs.kuleuven.be:8080/qbike/upload"
     fotodata = test 
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'} 
+    r = requests.post(url, data = test, headers = headers)
+    socketIO.emit('rt-sensordata', data,on_response)
