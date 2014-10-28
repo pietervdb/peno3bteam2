44218a7e3773dd600d27b740f7b240e04294410d
nathanvl <nathan.vanlaere@student.kuleuven.be>
2014-10-28 14:35:47 +0000

    aanpassing in gpstester

--- Arduino + RPi/GPS/GPS_tester.py
+++ Arduino + RPi/GPS/GPS_tester.py
@@ -1,31 +1,46 @@
 import gps, os, time
-
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 session = gps.gps()
+GPIO.setup(7,GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)
 
 while 1:
-    os.system('clear')
-    session.query('admosy') 
-    # a = altitude, d = date/time, m=mode,  
-    # o=postion/fix, s=status, y=satellites
+    GPIO.output(11,True)
+    GPIO.output(7,False)
 
-    print
-    print ' GPS reading'
-    print '----------------------------------------'
-    print 'latitude    ' , session.fix.latitude
-    print 'longitude   ' , session.fix.longitude
-    print 'time utc    ' , session.utc, session.fix.time
-    print 'altitude    ' , session.fix.altitude
-    print 'eph         ' , session.fix.eph
-    print 'epv         ' , session.fix.epv
-    print 'ept         ' , session.fix.ept
-    print 'speed       ' , session.fix.speed
-    print 'climb       ' , session.fix.climb
+    print "searching..."
+    if len(session.satellites)>0:
+        print "sattelites found!!"
+        os.system('clear')
+        #session.query('admosy') 
+        # a = altitude, d = date/time, m=mode,  
+        # o=postion/fix, s=status, y=satellites
+
+        print
+        print ' GPS reading'
+        print '----------------------------------------'
+        print 'latitude    ' , session.fix.latitude
+        print 'longitude   ' , session.fix.longitude
+        print 'time utc    ' , session.utc, session.fix.time
+        print 'altitude    ' , session.fix.altitude
+        #print 'eph         ' , session.fix.eph
+        #print 'epv         ' , session.fix.epv
+        #print 'ept         ' , session.fix.ept
+        print 'speed       ' , session.fix.speed
+        print 'climb       ' , session.fix.climb
+        
+        print
+        print ' Satellites (total of', len(session.satellites) , ' in view)'
+        for i in session.satellites:
+            print '\t', i
+        print session.fix.latitude,type(session.fix.latitude)
+
+        print session.fix.speed,type(session.fix.speed)
+        GPIO.output(11, False)
+        GPIO.output(7,True)
     
-    print
-    print ' Satellites (total of', len(session.satellites) , ' in view)'
-    for i in session.satellites:
-        print '\t', i
-
     time.sleep(3)
 
 
