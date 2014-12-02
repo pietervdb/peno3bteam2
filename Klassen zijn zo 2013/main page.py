299960660fb12efefec183394259acd04a2264bc
nathanvl <nathan.vanlaere@student.kuleuven.be>
2014-12-02 14:10:40 +0100

    pi

--- Klassen zijn zo 2013/main page.py
+++ Klassen zijn zo 2013/main page.py
@@ -1,10 +1,10 @@
 from photography import *
+print 'photography'
 from saving import *
-from sending import *
+print 'saving'
 
 import RPi.GPIO as GPIO
 import time
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(7,GPIO.IN)
 GPIO.setwarnings(False)
@@ -12,17 +12,22 @@ GPIO.setup(8,GPIO.OUT)
 GPIO.setup(11,GPIO.OUT)
 GPIO.output(8,False)
 GPIO.output(11,True)
-
 on = False
-time_last = time.time()
+timer_last = time.time()
 tripnumber = '0'
 queue = []
+photonumber = 0
 
 while True:
-    timer_current = time.time()
 
     if GPIO.input(7) == 1:
+        duration = 0
+        starter = time.time()
+        time.sleep(1)
+        while GPIO.input(7) == 1 and duration < 5:
+            duration = time.time()- starter
         if on:
+            print 'uit'
             on = False
             GPIO.output(11,True)
             GPIO.output(8,False)
@@ -30,24 +35,32 @@ while True:
             queue.append(tripnumber)
             time.sleep(1)
 
-            while not sent and GPIO.input(7) != 1:
+        else:
+            if duration >= 5:
+                from sending import *   #here because does not work without connection
                 if connected():
+                    
+                    print 'send'
                     GPIO.output(8,True) #do not disconnect-color
                     send_queue(queue)  #sends the queue
                     GPIO.output(8,False)
                     sent = True
-                    queue = []
-
-        else:
-            on = True
-            GPIO.output(11,False)
-            GPIO.output(8, True)
-            tripnumber = str(int(tripnumber)+1)
-            make_photo_dir(tripnumber)  #makes new directory
-            snap()
-            save_arduino(tripnumber,True)
-
+                queue = []
+            else:
+                print 'aan'
+                on = True
+                GPIO.output(11,False)
+                GPIO.output(8, True)
+                tripnumber = str(int(tripnumber)+1)
+                make_photo_dir(tripnumber)  #makes new directory
+                snap(0)
+                photonumber = 1
+                save_arduino(tripnumber,True)
+    
+    timer_current = time.time()
     if on and (timer_current - timer_last > 10):
-        snap()  #takes picture
+        print 'datapunt'
+        snap(photonumber)  #takes picture
+        photonumber += 1
         save_arduino(tripnumber,False)  #saves arduino data
         timer_last = time.time()
--- Klassen zijn zo 2013/sending.py
+++ Klassen zijn zo 2013/sending.py
@@ -4,24 +4,31 @@ import requests
 import os
 from socketIO_client import SocketIO
 from reading import *
-
+import urllib2
 domain = 'dali.cs.kuleuven.be'
 port = 8080
 start = {'purpose':'batch-sender','groupID':'CWB2','userID':'r0369676'}
 socketIO = SocketIO(domain,port)
 userID = "r0369676"
-groupID = "CWB2"
+groupID = "cwb2"
 answer = 0
 tripID = ''
-
-def connected():
+print 'checked'
+def connected2():
     "checks whether the pi is connected tot the internet"
     try:
         _ = requests.get(url='http://'+domain+':'+str(port)+'/', timeout=5)
         return True
     except requests.ConnectionError:
         return False
-    
+
+def connected():
+    try:
+        response=urllib2.urlopen('http://74.125.228.100', timeout=1)
+        return True
+    except urllib2.URLError as err: pass
+    return False
+print 'checked2'
 def on_response(*args):
     global answer
     print 'server message is',args
@@ -36,15 +43,23 @@ def send_data(tripnumber):
     "send the trips' data"
     global tripID
     socketIO.on('server_message',on_response)
+    socketIO.wait(2)
     socketIO.emit('start',json.dumps(start),on_response)
+    socketIO.wait(2)
     
     read(tripnumber)
     start_time = find_time('start')
     end_time = find_time('stop')
     datalist = make_data_list()
     meta_dict = make_meta_dict()
+    if start_time == None:
+        socketIO.emit('batch-tripdata', json.dumps([{'userID':userID,\
+                    'groupID':groupID,\
+                    'sensorData':datalist,\
+                    'meta':meta_dict}]),on_response)
     
-    socketIO.emit('batch-tripdata', json.dumps([{'userID':userID,\
+    else:
+        socketIO.emit('batch-tripdata', json.dumps([{'userID':userID,\
                     'groupID':groupID,'startTime':start_time,\
                     'endTime':end_time,'sensorData':datalist,\
                     'meta':meta_dict}]),on_response)
